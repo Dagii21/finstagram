1f2979992dae2c7596b37b8baddd7f5d8f6c6620
Dagii21 <dagii21.jamts@gmail.com>
2021-12-04 23:45:09 +0000

    W4D1

--- app/actions.rb
+++ app/actions.rb
@@ -6,53 +6,24 @@ def humanized_time_ago(time_ago_in_minutes)
   end
 end
 
-get '/' do
-  @finstagram_post_shark = {
-    username: "sharky_j",
-    avatar_url: "http://naserca.com/images/sharky_j.jpg",
-    photo_url: "http://naserca.com/images/shark.jpg",
-    humanized_time_ago: humanized_time_ago(15),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "sharky_j",
-      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-    }]
-  }
-
-  @finstagram_post_whale = {
-    username: "kirk_whalum",
-    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-    photo_url: "http://naserca.com/images/whale.jpg",
-    humanized_time_ago: humanized_time_ago(65),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "kirk_whalum",
-      text: "#weekendvibes"
-    }]
-  }
-
-  @finstagram_post_marlin = {
-    username:"marlin_peppa",
-    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-    photo_url: "http://naserca.com/images/marlin.jpg",
-    humanized_time_ago: humanized_time_ago(190),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "marlin_peppa",
-      text: "lunchtime! ;)"
-    }]
-  }
-
-  @finstagram_posts =[@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
-  # finstagram_post.to_s
-  
-  erb(:index)
-
-end
  
 get '/login' do    # when a GET request comes into /login
   erb(:login)      # render app/views/login.erb
+
+end
+
+get '/' do
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+#post '/signup' do
+#  "Form submitted!"
+#end
+post '/signup' do
+  params.to_s
 end
