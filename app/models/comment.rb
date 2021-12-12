3de478f0c6fd08b6b94025b3c229d1aef6cae980
Dagii21 <dagii21.jamts@gmail.com>
2021-12-12 00:38:27 +0000

    W6D1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :finstagram_post
+    # onoodriiin shine
+    validates_presence_of :text, :user, :finstagram_post
+    
 end
